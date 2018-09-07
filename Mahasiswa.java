4caa70e30b5c31c9c313fd066735298e9a62fcb8
wilda paras <wildasti98@gmail.com>
2018-09-07 21:16:24 +0700

    Initial commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,15 @@
+public class Mahasiswa{
+	public static void main(String[]args){
+		
+	}
+	
+	public String getNama(){
+		System.out.println(this.nama);
+	}
+	public void setNama(){
+		String nama;
+		this.nama = nama;
+	}
+	
+	
+}
